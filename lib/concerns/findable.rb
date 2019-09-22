8f648a75ae0555c5444d85b12eeda5a4a5a189f3
Joseph Sesti <sestijoey63@gmail.com>
2019-09-22 18:44:42 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,10 +1,9 @@
 module Findable
   module ClassMethods
   
-  @@all = all
-  
-  def find_by_name(name)
-    self..detect{|a| a.name == name}
+    def find_by_name(name)
+      self..detect{|a| a.name == name}
+    end
   end
 end
   
